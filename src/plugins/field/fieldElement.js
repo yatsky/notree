0a98e4ebe97e238e90d647e74b9b7f295dd3a77a
Yaoni <y.wang7@uq.net.au>
2021-10-02 22:51:29 +1000

    Set up field stub.

--- src/plugins/field/fieldElement.js
+++ src/plugins/field/fieldElement.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useEditorRef } from '@udecode/plate-core';
 import {ReactEditor} from "slate-react";
 import {setNodes} from "@udecode/plate-common";
@@ -7,41 +7,102 @@ export const FieldElement = (props) => {
     // https://docs.slatejs.org/concepts/09-rendering
     const { attributes, children, element } = props;
     const editor = useEditorRef();
+    const [fieldHidden,setFieldHidden ] = useState(false)
+
+    const isValidFieldName = (val) => {
+        return !val.includes(" ");
+    }
+    const onFieldNameChange = (e) => {
+        let val = e.target.value
+        if (isValidFieldName(val)){
+            const path = ReactEditor.findPath(editor, element);
+            setNodes(
+                editor,
+                { fieldName: val },
+                {at: path}
+            )
+        } else {
+            alert("White space is not allowed here")
+        }
+    }
 
     return (
         // Need contentEditable=false or Firefox has issues with certain input types.
         <div {...attributes} contentEditable={false}>
             <div style={{boxShadow: '0 0 0 3px #ddd', padding: '8px'}}>
-                <h4>Name:</h4>
+                <div>
+
+                <label>Field Name: </label>
                 <input
                     style={{margin: '8px 0'}}
                     type="text"
                     value={element.fieldName}
-                    onChange={(e) => {
-                        const path = ReactEditor.findPath(editor, element);
-                        setNodes(
-                            editor,
-                            { fieldName: e.target.value },
-                            {at: path}
-                        )
-                    }}
+                    onChange={onFieldNameChange}
                 />
-                <h4>Left or right handed:</h4>
+                </div>
+                <div>
+
+                <p>Model: </p>
                 <input
                     style={{width: 'unset'}}
                     type="radio"
-                    name="handedness"
-                    value="left"
+                    name="model"
+                    value="subsession"
                 />{' '}
-                Left
+                Subsession
                 <br/>
                 <input
                     style={{width: 'unset'}}
                     type="radio"
-                    name="handedness"
-                    value="right"
+                    name="model"
+                    value="group"
                 />{' '}
-                Right
+                Group
+                <br/>
+                <input
+                    style={{width: 'unset'}}
+                    type="radio"
+                    name="model"
+                    value="player"
+                />{' '}
+                Player
+                </div>
+                <div>
+                    <h4>
+                        Field type
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+
+                    Field widget
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+                        Choices
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+                        Initial value
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+                        Can this field be blank?
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+                        Field documentation
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+                        Field label
+                    </h4>
+                </div>
             </div>
             {children}
         </div>
