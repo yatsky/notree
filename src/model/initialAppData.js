f3b46342334778220a60262d2485ea65bb5976f7
Yaoni <y.wang7@uq.net.au>
2021-10-08 14:40:44 +1000

    Remove quotes around property key.

--- src/model/initialAppData.js
+++ src/model/initialAppData.js
@@ -2,24 +2,23 @@ import {initialValueBasicElements} from "../plugins/pluginsBasic";
 import {v4 as uuidv4} from 'uuid';
 
 export const initialAppData = {
-    "otherInfo": "information",
-    "pagesData": [
+    otherInfo: "information",
+    pagesData: [
         {
-            "pageId": uuidv4(),
-            "pageName": "Introduction",
-            "selected": true,
-            "nameReadOnly": true,
-            "deleted": false,
-            "nodes": initialValueBasicElements,
+            pageId: uuidv4(),
+            pageName: "Introduction",
+            selected: true,
+            nameReadOnly: true,
+            deleted: false,
+            nodes: initialValueBasicElements,
         },
         {
-            "pageId": uuidv4(),
-            "pageName": "Survey",
-            "selected": false,
-            "nameReadOnly": true,
-            "deleted": false,
-            "nodes": initialValueBasicElements,
-        }
-
+            pageId: uuidv4(),
+            pageName: "Introduction",
+            selected: true,
+            nameReadOnly: true,
+            deleted: false,
+            nodes: initialValueBasicElements,
+        },
     ]
 }
