85b85fc02dd361bf69f2e509b89b86696bbfc2f2
Yi Wang <yi@customology.com.au>
2021-10-16 14:18:48 +1000

    Set up jest and connect to amplify.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sign in to your account/i);
   expect(linkElement).toBeInTheDocument();
 });
