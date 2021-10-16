b9cf84192a1802ddee6324b4cf8378ec6c1badf6
Yaoni <y.wang7@uq.net.au>
2021-10-16 15:24:15 +1000

    First successful test setup!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Sign in to your account/i);
+test('renders learn react link', async () => {
+    await act(async () => render(<App />));
+  const linkElement = screen.getByText(/Publish/i);
   expect(linkElement).toBeInTheDocument();
 });
