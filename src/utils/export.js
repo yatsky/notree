324bd24ec5bbcad98dc9a8665969be765495e2c5
Yaoni <y.wang7@uq.net.au>
2021-10-24 15:13:05 +1000

    Export pages with correct page names.

--- src/utils/export.js
+++ src/utils/export.js
@@ -1,34 +1,44 @@
-import { html } from "js-beautify";
-import { serializeHTMLFromNodes } from "@udecode/plate-html-serializer";
-import { createEditorPlugins } from "@udecode/plate";
+import {html} from "js-beautify";
+import {serializeHTMLFromNodes} from "@udecode/plate-html-serializer";
+import {createEditorPlugins} from "@udecode/plate";
 import FileSaver from "file-saver";
 
 export const handleExport = async (plugins, pagesData) => {
-  // setLoadingExport(true)
-  // Why plugins and parser: https://github.com/prettier/prettier/pull/6268#issue-294147726
-  // let text = prettier.format(serializeHTMLFromNodes({ plugins: plugins, nodes: editor.children }), {
-  //   parser: 'html',
-  //   plugins: [parserHTML],
-  // })
-  let text = pagesData.map((pageData) =>
-    html(
-      serializeHTMLFromNodes(createEditorPlugins(plugins), {
-        plugins: plugins,
-        nodes: pageData.nodes,
-        preserveClassNames: ["slate-", "notree-"],
-      })
-    )
-  );
-  for (const el of text) {
-    let regex = /{{/gm;
-    let subst = `{%`;
-    let result = el.replace(regex, subst);
-    regex = /}}/gm;
-    subst = `%}`;
-    result = result.replace(regex, subst);
+    // setLoadingExport(true)
+    // Why plugins and parser: https://github.com/prettier/prettier/pull/6268#issue-294147726
+    // let text = prettier.format(serializeHTMLFromNodes({ plugins: plugins, nodes: editor.children }), {
+    //   parser: 'html',
+    //   plugins: [parserHTML],
+    // })
+    // https://stackoverflow.com/questions/34398279/map-and-filter-an-array-at-the-same-time
+    let pageHTMLs = pagesData.reduce((reduced, pageData) => {
+            if (!pageData.deleted) {
+                reduced.push(
+                    {
+                        pageName: pageData.pageName,
+                        html: html(
+                            serializeHTMLFromNodes(createEditorPlugins(plugins), {
+                                plugins: plugins,
+                                nodes: pageData.nodes,
+                                preserveClassNames: ["slate-", "notree-"],
+                            })
+                        )
+                    }
+                )
+            }
+            return reduced
+        }, []
+    );
+    for (const el of pageHTMLs) {
+        let regex = /{{/gm;
+        let subst = `{%`;
+        let result = el.html.replace(regex, subst);
+        regex = /}}/gm;
+        subst = `%}`;
+        result = result.replace(regex, subst);
 
-    let blob = new Blob([result], { type: "text/html;charset=utf-8" });
-    await FileSaver.saveAs(blob, "notree-download.html");
-    // setLoadingExport(false)
-  }
+        let blob = new Blob([result], {type: "text/html;charset=utf-8"});
+        await FileSaver.saveAs(blob, `${el.pageName}.html`);
+        // setLoadingExport(false)
+    }
 };
